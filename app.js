95a38c126c6be1b7bdd46cc1f034514081232ae8
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-04-07 06:08:52 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// let title = document.querySelector("#title");
+
+// let minute = 0;
+// let seconds = 0;
+// let hours = 0;
+
+// const interval = setInterval(() => {
+//   seconds += 1;
+//   title.textConten = `${minute < 10 ? `0${minute}` : minute} : ${
+//     seconds < 10 ? `0${seconds}` : seconds
+//   }`;
+//   if (seconds == 59) {
+//     seconds = 0;
+//     minute += 1;
+//   }
+// }, 1000);
